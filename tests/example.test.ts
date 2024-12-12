5f6d31cbc767d52a27295ce7d7023d80a95673db
lumen-limitless <lumenlimitless@gmail.com>
2024-12-12 15:05:13 -0500

    biome check and formatting

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,5 +1,8 @@
+import assert from 'node:assert';
 import { describe, it } from 'node:test';
 
 describe('My Test Suite', () => {
-  it('should do something', () => {});
+  it('should do something', () => {
+    assert.equal(1, 1);
+  });
 });
